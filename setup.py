06dfb25810864fa0ca3ce512342642f74b25248a
M Akutagawa <makutaga@tokushima-u.ac.jp>
2023-01-30 23:43:15 +0900

    setup.py is added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='eegtools',
+        version='0.1',
+        description="various eeg tools",
+        long_description="",
+        author="makutaga",
+        license="MIT",
+        classifiers=[
+            "Development Status :: 1 - Plannning"
+            ]
+        )
