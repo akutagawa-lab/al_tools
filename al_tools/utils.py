bbc6107689a9bc33a8b98b5729b29a865d002f00
M Akutagawa <makutaga@tokushima-u.ac.jp>
2023-01-31 11:52:27 +0900

    comment is added

--- al_tools/utils.py
+++ al_tools/utils.py
@@ -8,6 +8,20 @@ import pandas as pd
 
 def read_wav(filename):
     ''' WAV ファイルの読み込み
+
+    Args:
+        filename (str or Path): wav filename
+
+    Returns:
+        params (dict): result
+            params['df'] (Pandas.DataFrame): audio data
+                In case of multiple channel wav file, columns are named as
+                'ch_1', 'ch_2', ...
+                Time represented as second is stored in column named 'time'.
+                i.e. params['df']['time'] represents time stamp of each row.
+            params['path'] (str): wav filename
+            params['fs'] (float): sampling frequency
+            params['nchannels'] (int): number of channels
     '''
     params = {}
 
@@ -41,21 +55,47 @@ def read_wav(filename):
     return params
 
 
-def read_eeg(filename):
-    ''' EEGファイルの読み込み
+def read_eeg(filename, time_col='time'):
+    ''' Polymate CSVファイルの読み込み
 
-    Polymate の CSV フォーマットに対応
+    Args:
+        filename (str or Path): filename to be read.
+        time_col (str): column name of time
+
+    Returns:
+        params (dict): result to read
+        params['df'] (Pandas.DataFrame): eeg data
+        params['path'] (str or Path): file path
+        params['fs'] (float): sampling rate
+        params['orig_columns'] (list): original column name
+        params['df_head'] (Pandas.DataFrame): header information
+            params['df_head'][<name>]['Type']: channel type
+            params['df_head'][<name>]['Unit']: unit
     '''
     params = {}
 
     df = pd.read_csv(filename, header=3)
     df.rename(columns={'TIME': 'TIME_str'}, inplace=True)
-    fs = pd.read_csv(filename, header=None, nrows=1)[1][0]
-    df['time'] = np.arange(df.shape[0]) / fs
+    params['orig_columns'] = df.columns.to_list()
 
+    # read sampling rate
+    fs = pd.read_csv(filename, header=None, nrows=1)[1][0]
+    df[time_col] = np.arange(df.shape[0]) / fs
+
+    # read additional information for each column
+    df_head = pd.read_csv(filename, header=None, skiprows=1, nrows=3)
+    df_head = df_head.set_axis(df_head.iloc[2].to_list(), axis=1)
+    df_head.rename(columns={'TIME': 'TIME_str'}, inplace=True)
+    df_head.drop(df.index[[2]], inplace=True)
+    df_head = df_head.set_axis(['Type', 'Unit'], axis=0)
+    df_head['TIME_str']['Unit'] = 's'
+    df_head['TIME_str']['Type'] = ''
+    df_head[time_col] = ''
+    df_head[time_col]['Type'] = ''
+    df_head[time_col]['Unit'] = 's'
     params['df'] = df
     params['path'] = filename
     params['fs'] = fs
+    params['df_head'] = df_head
     return params
-
 
